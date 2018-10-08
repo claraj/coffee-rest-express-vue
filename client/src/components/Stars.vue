63b36a45decc180446106f6a7915faf9a271ffd6
clara <clara@minneapolis.edu>
2018-10-08 09:20:34 -0500

    fix prop names

--- client/src/components/Stars.vue
+++ client/src/components/Stars.vue
@@ -1,9 +1,10 @@
 <template>
   <div v-bind:id="'star-'+_id">
-    <input class="star-range-slider" v-model="starsSet" v-on:input="onStarsChanged" type="range" min="1" max="5"/>
+    <!-- <input class="star-range-slider" v-model="starsSet" v-on:input="onStarsChanged" type="range" min="1" max="5"/> -->
+    <input class="star-range-slider" v-model="starsSet" type="range" v-on:input="onStarsChanged" min="1" max="5"/>
     <span class="star-text-display">
-      <span class="star-value">{{stars}}</span>
-      <span v-if="stars==1">star</span>
+      <span class="star-value">{{starsSet}}</span>
+      <span v-if="starsSet==1">star</span>
       <span v-else>stars</span>
     </span>
   </div>
@@ -17,7 +18,7 @@ export default {
     stars: Number, // initial number of stars, set by parent
     _id: String // _id of thing that has this number of stars
   },
-  data() {
+  data () {
     return {
       starsSet: this.stars // This component's local copy, used to set value of range slider.
     }
--- client/test/unit/specs/Stars.spec.js
+++ client/test/unit/specs/Stars.spec.js
@@ -1,4 +1,3 @@
-import Vue from 'vue'
 import Stars from '@/components/Stars'
 import { mount } from '@vue/test-utils'
 
@@ -6,30 +5,31 @@ describe('Stars.vue', () => {
   it('should render a range slider with min = 1 and max = 5', () => {
     const wrapper = mount(Stars, { propsData: {stars: 4, _id: '2'} })
     const slider = wrapper.find('.star-range-slider')
-    expect(slider.attributes('min')).to.be.equal("1")
-    expect(slider.attributes('max')).to.be.equal("5")
-    expect(slider.element.value).to.be.equal("4")
+    expect(slider.attributes('min')).to.be.equal('1')
+    expect(slider.attributes('max')).to.be.equal('5')
+    expect(slider.element.value).to.be.equal('4')
   })
 
   it('should render the text "3 stars" if the value is 3', () => {
-    const wrapper = mount(Stars, { propsData: {stars: 3, _id: '2'} })
+    const wrapper = mount(Stars, {propsData: {stars: 3, _id: '2'} })
     const starText = wrapper.find('.star-text-display')
     expect(starText.text()).to.be.equal('3 stars')
   })
 
   it('should render the text "1 star" if the value is 1', () => {
-    const wrapper = mount(Stars, { propsData: {stars: 1, _id: '2'} })
+    const wrapper = mount(Stars, {propsData: {stars: 1, _id: '2'} })
     const starText = wrapper.find('.star-text-display')
     expect(starText.text()).to.be.equal('1 star')
   })
 
   it('should update the text number of stars on when slider is moved', () => {
-    const wrapper = mount(Stars)
-    const starText = wrapper.find('.star-text-display')
+    const wrapper = mount(Stars, {propsData: {stars: 1}})
     const starRange = wrapper.find('.star-range-slider')
-
-    starRange.element.value = 2
+    // starRange.element.value = 2  // This is equivalent to the long below
+    starRange.setValue(2)
     starRange.trigger('input')
+
+    const starText = wrapper.find('.star-text-display')
     expect(starText.text()).to.be.equal('2 stars')
 
     starRange.element.value = 5
@@ -38,11 +38,10 @@ describe('Stars.vue', () => {
   })
 
   it('should emit event when slider is moved', () => {
-    const wrapper = mount(Stars, { propsData: {_id: '3'}})
+    const wrapper = mount(Stars, {propsData: {_id: '3'}})
     const starRange = wrapper.find('.star-range-slider')
     starRange.element.value = 2
-    starRange.trigger('input') //', { target: { value: 2 } })
-    console.log('the emitted event is', wrapper.emitted('onStarsChanged'))
+    starRange.trigger('input')
     expect(wrapper.emitted('onStarsChanged').length).to.be.equal(1)
     expect(wrapper.emitted('onStarsChanged')).to.be.eql([['3', 2]])
   })
