ee2b90b3e4044e6dc5994f7c31a695888da9b2a4
clara <clara@minneapolis.edu>
2018-10-09 16:10:50 -0500

    server tests

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const serveStatic = require('serve-static')
 const path = require('path')
 const dbConfig = require('./db_config')
 
-console.log(dbConfig)
+console.log('the database to use is ', dbConfig)
 const staticPath = path.join(__dirname, '..', 'client', 'dist')
 
 console.log(staticPath)
@@ -19,13 +19,24 @@ app.use(bodyParser.json())
 app.use(methodOverride())
 
 mongoose.connect(dbConfig)
+  .then(() => console.log('Connected'))
+  .catch(err => { console.log(err) })
 
 const coffeeShop = require('./models/coffeeShop')
 
-restify.serve(router, coffeeShop)
+restify.serve(router, coffeeShop, { onError: (err, req, res, next) => {
+  const statusCode = req.erm.statusCode
+  console.log(err)
+  console.log(err.message)
+  res.status(statusCode).json({
+    message: err.message
+  })
+}})
 
 app.use(router)
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Express server on port 3000')
 })
+
+module.exports = app
